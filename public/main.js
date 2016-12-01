ea4ba6a69e3018464b3aec1d5aef562abb741511
Sean W. Mahan <smahan@knowledge-architecture.com>
2016-12-01 14:52:44 -0500

    Better test page stuff, renamed event, temporarily adding port to url

--- public/main.js
+++ public/main.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var socket = io.connect(window.location.hostname, {
+  var socket = io.connect(window.location.hostname + ":3000", {
     query: 'notificationKey=NOTIFICATION_KEY'
   });
-  socket.on('NEW_NOTIFICATION', function (notification) {
+  socket.on('NOTIFICATION', function (notification) {
+	  document.getElementById("notification_display").innerHTML = notification;
     console.log(notification);
   });
 
